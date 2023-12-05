ae6c39d4237fa1a166d457f56bfcd4d2a5284c1a
jvrabelo <106701350+jvrabelo@users.noreply.github.com>
2023-12-05 13:05:30 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -98,9 +98,9 @@ winnerSelected.innerHTML = winner;
 }
 
 function changesColorSquare(square1, square2, square3) {
-square1.style.background = '#	0000FF';
-square2.style.background = '#	0000FF';
-square3.style.background = '#	0000FF';
+square1.style.background = '#0000FF';
+square2.style.background = '#0000FF';
+square3.style.background = '#0000FF';
 }
 
 function checkSequence(square1, square2, square3) {
